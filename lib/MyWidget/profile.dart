01b3b749fe4015d5a891cd4c03d19e78be75250f
Epiphane <omarepiphanefarouk@gmail.com>
2025-07-08 04:53:28 +0100

    Omar

--- lib/MyWidget/profile.dart
+++ lib/MyWidget/profile.dart
@@ -10,28 +10,100 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.chevron_left),
-                  iconSize: 30,
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                Text(
-                  "Mon Profil",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Center(
+            child: Text(
+              "Profile",
+              style: TextStyle(
+                fontSize: 18,
+                color: Color(0xFF202244),
+                fontFamily: 'Jost',
+                fontWeight: FontWeight.w600,
+                height: 1.0,
+                letterSpacing: 0.0,
+              ),
             ),
-            // Ajoutez ici les éléments de votre profil
-          ],
-        ),
+          ),
+
+          SizedBox(height: 40),
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/images/ilimi_logo.png'),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "Naphiou Dan Salao",
+            style: TextStyle(
+              fontSize: 16,
+              color: Color(0xFF202244),
+              fontFamily: 'Jost',
+              fontWeight: FontWeight.w600,
+              height: 1.0,
+              letterSpacing: 0.0,
+            ),
+          ),
+
+          SizedBox(height: 10),
+          Text(
+            "naphsalao@gmail.com",
+            style: TextStyle(
+              fontSize: 10,
+              color: Color(0xFF202244),
+              fontFamily: 'Jost',
+              fontWeight: FontWeight.w600,
+              height: 1.0,
+              letterSpacing: 0.0,
+            ),
+          ),
+          SizedBox(height: 40),
+          Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Modifier le profil"),
+                trailing: Icon(Icons.chevron_right),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.school),
+                title: Text("Devenir formateur"),
+                trailing: Icon(Icons.chevron_right),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.notifications),
+                title: Text("Notifications"),
+                trailing: Icon(Icons.chevron_right),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.language),
+                title: Text("Langue"),
+                trailing: Icon(Icons.chevron_right),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.help),
+                title: Text("Aide et support"),
+                trailing: Icon(Icons.chevron_right),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text("À propos de l'application"),
+                trailing: Icon(Icons.chevron_right),
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Déconnexion", style: TextStyle(color: Colors.red)),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
